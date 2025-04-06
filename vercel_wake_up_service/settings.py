72116a0def7e2b03d39976a57a48b49e881be53a
KING143ND <nvnaveenchaudhary@gmail.com>
2025-04-06 14:34:21 +0530

    Update csrf trusted origin with add migrate command in docker

--- vercel_wake_up_service/settings.py
+++ vercel_wake_up_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
 DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
-
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
